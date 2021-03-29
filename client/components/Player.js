b1645f5cbde4723f78fd206298e123d822e593fb
Nathan Damico <nathandamico5@gmail.com>
2021-03-29 12:57:32 -0400

    Added Single Song Row Component

--- client/components/Player.js
+++ client/components/Player.js
@@ -1,21 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Player = () => {
-  return (
-    <div id="player-container">
-      <div id="player-controls">
-        <div className="back">
-          <img src="/back.png" />
+class Player extends React.Component {
+  render() {
+    return (
+      <div id="player-container">
+        <div id="player-controls">
+          <div className="back">
+            <img src="/back.png" />
+          </div>
+          <div className="play-pause">
+            <img src="/play.png" />
+          </div>
+          <div className="next">
+            <img src="/forward.png" />
+          </div>
         </div>
-        <div className="play-pause">
-          <img src="/play.png" />
-        </div>
-        <div className="next">
-          <img src="/forward.png" />
-        </div>
+        {this.props.selectedSong.audioUrl && (
+          <audio src={this.props.selectedSong.audioUrl}></audio>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Player;
+const mapStateToProps = (state) => ({
+  selectedSong: state.songsData.selectedSong,
+});
+
+export default connect(mapStateToProps)(Player);
